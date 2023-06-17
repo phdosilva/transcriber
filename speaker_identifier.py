0ca022d589baf823c9f56b0566c1f4d7a7b8f3af
pedro henrique <phos@cin.ufpe.br>
2023-06-17 20:36:46 -0300

    adding async and prints to debug

--- speaker_identifier.py
+++ speaker_identifier.py
@@ -4,15 +4,21 @@ import os
 
 class Diarization:
     def __init__(self):
+        print("Initializing pipeline")
         self.pipeline: Callable = Pipeline.from_pretrained("pyannote/speaker-diarization@2.1",
                                                            use_auth_token=os.environ.get("HUGGINGFACE_TOKEN"))
 
-    def start(self, filename) -> None:
+    async def start(self, filename) -> None:
+        print(f"Starting diarization at {filename}")
         try:
             DEMO_FILE = {"uri": "blabal", "audio": f"./handled_files/{filename}"}
             diarization = self.pipeline(DEMO_FILE)
 
             with open(f"./handled_files/diarization.txt", "wb") as text_file:
                 text_file.write(diarization)
+            print("Success with diarization")
         except Exception as error:
+            print("Something went wrong")
             raise Exception("There is an error with diarization", error.args)
+        finally:
+            print("Diarization ended")
