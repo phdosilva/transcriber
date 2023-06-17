776fca0d744661a00713012296dd8a19a2291c65
pedro henrique <phos@cin.ufpe.br>
2023-06-17 19:27:10 -0300

    async in diarization

--- speaker_identifier.py
+++ speaker_identifier.py
@@ -7,7 +7,7 @@ class Interview:
         self.pipeline: Callable = Pipeline.from_pretrained("pyannote/speaker-diarization@2.1",
                                                            use_auth_token=os.environ.get("HUGGINGFACE_TOKEN"))
 
-    def diarization(self, filename) -> None:
+    async def diarization(self, filename) -> None:
         try:
             DEMO_FILE = {"uri": "blabal", "audio": f"./handled_files/{filename}"}
             diarization = self.pipeline(DEMO_FILE)
@@ -16,5 +16,3 @@ class Interview:
                 text_file.write(diarization)
         except Exception as error:
             raise Exception("There is an error with diarization", error.args)
-
-        
